714b0101e84314c9b16b392031983b2dbc3bf1db
hoseec27 <hoseec27@gmail.com>
2020-06-26 01:07:16 +0800

    localnet commmit: add sizeofvariable

---
+++ size_of_variable01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  fprintf(stdout, "Testing \n\n");
+  fprintf(stdout, "size of int = %d \n", sizeof(int) );
+  fprintf(stdout, "size of char = %d \n", sizeof(char) );
+  fprintf(stdout, "size of cs01 = %d \n", sizeof(char[100]) );
+  fprintf(stdout, "size of float = %d \n", sizeof(float) );
+  fprintf(stdout, "size of double = %d \n", sizeof(double) );
+  fprintf(stdout, "size of long double = %d \n", sizeof(long double) );
+
+  return 0;
+}
